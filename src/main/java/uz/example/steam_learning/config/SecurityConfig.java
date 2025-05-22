55fb5e9e806bc15295c7be8ef92700fef9ce5777
AbdualimovBotir <botirabdualimov07@gmail.com>
2025-05-22 12:23:26 +0500

    swager confirguration added

--- src/main/java/uz/example/steam_learning/config/SecurityConfig.java
+++ src/main/java/uz/example/steam_learning/config/SecurityConfig.java
@@ -75,47 +75,99 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .csrf().disable()
-                .formLogin().disable()
-                .httpBasic().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(new RestAuthenticationEntryPoint()).and()
-                .authorizeRequests()
-                .antMatchers(
-                        "/",
-                        "/error",
-                        "/favicon.ico",
-                        "/**/*.png",
-                        "/**/*.gif",
-                        "/**/*.svg",
-                        "/**/*.jpg",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js"
-                ).permitAll()
-                .antMatchers("/auth/**", "/oauth2/**").permitAll()
-                .antMatchers(
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html"
-                ).permitAll()
-                .anyRequest().authenticated().and()
-                .oauth2Login()
-                .authorizationEndpoint()
-                .baseUri("/oauth2/authorize")
-                .authorizationRequestRepository(cookieAuthorizationRequestRepository()).and()
-                .redirectionEndpoint()
-                .baseUri("/oauth2/callback/*").and()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService).and()
-                .successHandler(oAuth2AuthenticationSuccessHandler)
-                .failureHandler(oAuth2AuthenticationFailureHandler);
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .cors().and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//                .csrf().disable()
+//                .formLogin().disable()
+//                .httpBasic().disable()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(new RestAuthenticationEntryPoint()).and()
+//                .authorizeRequests()
+//                .antMatchers(
+//                        "/",
+//                        "/error",
+//                        "/favicon.ico",
+//                        "/**/*.png",
+//                        "/**/*.gif",
+//                        "/**/*.svg",
+//                        "/**/*.jpg",
+//                        "/**/*.html",
+//                        "/**/*.css",
+//                        "/**/*.js"
+//                ).permitAll()
+//                .antMatchers("/auth/**", "/oauth2/**").permitAll()
+//                .antMatchers(
+//                        "/v3/api-docs/**",
+//                        "/swagger-ui/**",
+//                        "/swagger-ui.html",
+//                        "/swagger-ui/index.html",
+//                        "/swagger-resources/**",
+//                        "/webjars/**"
+//                ).permitAll()
+//                .anyRequest().authenticated().and()
+//                .oauth2Login()
+//                .authorizationEndpoint()
+//                .baseUri("/oauth2/authorize")
+//                .authorizationRequestRepository(cookieAuthorizationRequestRepository()).and()
+//                .redirectionEndpoint()
+//                .baseUri("/oauth2/callback/*").and()
+//                .userInfoEndpoint()
+//                .userService(customOAuth2UserService).and()
+//                .successHandler(oAuth2AuthenticationSuccessHandler)
+//                .failureHandler(oAuth2AuthenticationFailureHandler);
+//
+//        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+//    }
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .cors().and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .csrf().disable()
+                    .formLogin().disable()
+                    .httpBasic().disable()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(new RestAuthenticationEntryPoint()).and()
+                    .authorizeRequests()
+                    .antMatchers(
+                            "/",
+                            "/error",
+                            "/favicon.ico",
+                            "/**/*.png",
+                            "/**/*.gif",
+                            "/**/*.svg",
+                            "/**/*.jpg",
+                            "/**/*.html",
+                            "/**/*.css",
+                            "/**/*.js",
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html",
+                            "/swagger-ui/index.html",
+                            "/swagger-resources/**",
+                            "/webjars/**"
+                    ).permitAll()
+                    .antMatchers(
+                            "/auth/**",
+                            "/oauth2/**"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .oauth2Login()
+                    .authorizationEndpoint()
+                    .baseUri("/oauth2/authorize")
+                    .authorizationRequestRepository(cookieAuthorizationRequestRepository()).and()
+                    .redirectionEndpoint()
+                    .baseUri("/oauth2/callback/*").and()
+                    .userInfoEndpoint()
+                    .userService(customOAuth2UserService).and()
+                    .successHandler(oAuth2AuthenticationSuccessHandler)
+                    .failureHandler(oAuth2AuthenticationFailureHandler);
 
-        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+            http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        }
+
 }
