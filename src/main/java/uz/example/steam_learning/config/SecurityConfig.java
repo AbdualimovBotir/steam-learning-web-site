f7d55301d3d68e4fcdea5a11941470a0b07797b4
AbdualimovBotir <botirabdualimov07@gmail.com>
2025-05-22 12:07:36 +0500

    swager added

--- src/main/java/uz/example/steam_learning/config/SecurityConfig.java
+++ src/main/java/uz/example/steam_learning/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import uz.example.steam_learning.security.oauth2.CustomOAuth2UserService;
 import uz.example.steam_learning.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
 import uz.example.steam_learning.security.oauth2.OAuth2AuthenticationFailureHandler;
 import uz.example.steam_learning.security.oauth2.OAuth2AuthenticationSuccessHandler;
+import uz.example.steam_learning.security.CustomUserDetailsService;
+import uz.example.steam_learning.security.RestAuthenticationEntryPoint;
+import uz.example.steam_learning.security.TokenAuthenticationFilter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +22,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import uz.example.steam_learning.security.CustomUserDetailsService;
-import uz.example.steam_learning.security.RestAuthenticationEntryPoint;
-import uz.example.steam_learning.security.TokenAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -51,11 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new TokenAuthenticationFilter();
     }
 
-    /*
-      By default, Spring OAuth2 uses HttpSessionOAuth2AuthorizationRequestRepository to save
-      the authorization request. But, since our service is stateless, we can't save it in
-      the session. We'll save the request in a Base64 encoded cookie instead.
-    */
     @Bean
     public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
         return new HttpCookieOAuth2AuthorizationRequestRepository();
@@ -73,7 +69,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -83,22 +78,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors()
-                    .and()
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                .csrf()
-                    .disable()
-                .formLogin()
-                    .disable()
-                .httpBasic()
-                    .disable()
+                .cors().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
                 .exceptionHandling()
-                    .authenticationEntryPoint(new RestAuthenticationEntryPoint())
-                    .and()
+                .authenticationEntryPoint(new RestAuthenticationEntryPoint()).and()
                 .authorizeRequests()
-                    .antMatchers("/",
+                .antMatchers(
+                        "/",
                         "/error",
                         "/favicon.ico",
                         "/**/*.png",
@@ -107,28 +96,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.jpg",
                         "/**/*.html",
                         "/**/*.css",
-                        "/**/*.js")
-                        .permitAll()
-                    .antMatchers("/auth/**", "/oauth2/**")
-                        .permitAll()
-                    .anyRequest()
-                        .authenticated()
-                    .and()
+                        "/**/*.js"
+                ).permitAll()
+                .antMatchers("/auth/**", "/oauth2/**").permitAll()
+                .antMatchers(
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html"
+                ).permitAll()
+                .anyRequest().authenticated().and()
                 .oauth2Login()
-                    .authorizationEndpoint()
-                        .baseUri("/oauth2/authorize")
-                        .authorizationRequestRepository(cookieAuthorizationRequestRepository())
-                        .and()
-                    .redirectionEndpoint()
-                        .baseUri("/oauth2/callback/*")
-                        .and()
-                    .userInfoEndpoint()
-                        .userService(customOAuth2UserService)
-                        .and()
-                    .successHandler(oAuth2AuthenticationSuccessHandler)
-                    .failureHandler(oAuth2AuthenticationFailureHandler);
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorize")
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository()).and()
+                .redirectionEndpoint()
+                .baseUri("/oauth2/callback/*").and()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService).and()
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler);
 
-        // Add our custom Token based authentication filter
         http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
